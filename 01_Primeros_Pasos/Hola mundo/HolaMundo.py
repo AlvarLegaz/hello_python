4dd53b3a22d31e945145157f6da7a417d89e139b
Álvar Legaz Aparicio <116595540+AlvarLegaz@users.noreply.github.com>
2024-08-31 17:53:15 +0200

    Update HolaMundo.py

--- 01_Primeros_Pasos/Hola mundo/HolaMundo.py
+++ 01_Primeros_Pasos/Hola mundo/HolaMundo.py
@@ -5,11 +5,5 @@ Created on Sun Mar 29 17:24:57 2020
 @author: Vonliendres
 """
 
-from Sistema import Esperas
-import tweepy
-
 print("Hola mundo")
-
-Esperas.espera_ms(5000)
-
 print("Programa ejecutado")
